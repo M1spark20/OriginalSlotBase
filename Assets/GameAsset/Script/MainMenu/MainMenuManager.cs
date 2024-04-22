8e657ad1b1c339b5ad53d54aaba5d88936215128
sin spark <spaaaark.sin120@gmail.com>
2024-04-22 23:02:39 +0900

    [ADD]メニューに項目選択用ボタンを追加
    ボタン内の文字はパネル名を自動反映する。

--- Assets/GameAsset/Script/MainMenu/MainMenuManager.cs
+++ Assets/GameAsset/Script/MainMenu/MainMenuManager.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenuManager : MonoBehaviour
 {
 	[SerializeField] private GameObject[] Panels;
+	[SerializeField] private Button SelectorBase;
 	
 	private int SelectedID;
+	private Button[] Selector;
 	
 	// 各パネルからキー入力情報を取得するための記録機能
 	private bool[] GetKeyDown;
@@ -18,7 +22,21 @@ public class MainMenuManager : MonoBehaviour
         GetKeyDown = new bool[(int)EMenuButtonID.eButtonMax];
         for(int i=0; i<GetKeyDown.Length; ++i) GetKeyDown[i] = false;
         
-        for(int i=0; i<Panels.Length; ++i) Panels[i].SetActive(false);
+        // 選択用ボタン配置
+        Selector = new Button[Panels.Length];
+        Selector[0] = SelectorBase;
+        float sizeX = SelectorBase.GetComponent<RectTransform>().sizeDelta.x;
+        
+        for(int i=0; i<Panels.Length; ++i) {
+        	Panels[i].SetActive(false);
+        	if (i > 0) {
+        		Selector[i] = Instantiate(SelectorBase, this.transform);
+        		Selector[i].transform.localPosition += new Vector3(sizeX * i, 0, 0);
+        	}
+        	Selector[i].transform.Find("name").GetComponent<TextMeshProUGUI>().text = Panels[i].name;
+        	var prm = i;	// 変数に入れないとデルタがうまく動かないらしい…
+        	Selector[i].onClick.AddListener(() => OnClickButton(prm));
+        }
         RefreshActivate(0);
     }
 
@@ -53,5 +71,10 @@ public class MainMenuManager : MonoBehaviour
     		if (activeID >= 0) Panels[activeID].SetActive(true);
     		SelectedID = activeID;
     	} 
+    }
+    
+    public void OnClickButton(int index){
+    	// ボタンを押したデータへ遷移する。
+    	RefreshActivate(index);
     }
 }
