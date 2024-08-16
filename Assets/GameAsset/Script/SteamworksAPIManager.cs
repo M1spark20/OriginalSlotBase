39a970de67270e8a9b677cc46aebd618df9d5502
sin spark <spaaaark.sin120@gmail.com>
2024-08-16 09:26:09 +0900

    [ADD]Steamworks APIを導入
    実装内容はこのあたりを参考に、サンプルを導入して確認した。
    https://github.com/rlabrecque/Steamworks.NET-Example/blob/master/Assets/Scripts/SteamStatsAndAchievements.cs#L257

---
+++ Assets/GameAsset/Script/SteamworksAPIManager.cs
@@ -0,0 +1,157 @@
+using UnityEngine;
+using System.Collections;
+using Steamworks;
+
+public class SteamworksAPIManager : MonoBehaviour {
+	// Our GameID
+	private CGameID m_GameID;
+	
+	// Did we get the stats from Steam?
+	private bool m_bRequestedStats;
+	private bool m_bStatsValid;
+
+	// Should we store stats this frame?
+	private bool m_bStoreStats;
+	
+	// Callbacks
+	protected Callback<UserStatsReceived_t> m_UserStatsReceived;
+	protected Callback<UserStatsStored_t> m_UserStatsStored;
+	protected Callback<UserAchievementStored_t> m_UserAchievementStored;
+		
+    void Start() {
+        if(SteamManager.Initialized) {
+            string name = SteamFriends.GetPersonaName();
+            Debug.Log(name);
+        }
+    }
+    
+	void OnEnable() {
+		if (!SteamManager.Initialized) return;
+
+		// Cache the GameID for use in the Callbacks
+		m_GameID = new CGameID(SteamUtils.GetAppID());
+
+		m_UserStatsReceived = Callback<UserStatsReceived_t>.Create(OnUserStatsReceived);
+		m_UserStatsStored = Callback<UserStatsStored_t>.Create(OnUserStatsStored);
+		m_UserAchievementStored = Callback<UserAchievementStored_t>.Create(OnAchievementStored);
+
+		// These need to be reset to get the stats upon an Assembly reload in the Editor.
+		m_bRequestedStats = false;
+		m_bStatsValid = false;
+	}
+	
+	private void Update() {
+		if (!SteamManager.Initialized) return;
+
+		if (!m_bRequestedStats) {
+			// Is Steam Loaded? if no, can't get stats, done
+			if (!SteamManager.Initialized) {
+				m_bRequestedStats = true;
+				return;
+			}
+			
+			// If yes, request our stats (この返答としてコールバックが呼ばれる?)
+			bool bSuccess = SteamUserStats.RequestCurrentStats();
+
+			// This function should only return false if we weren't logged in, and we already checked that.
+			// But handle it being false again anyway, just ask again later.
+			m_bRequestedStats = bSuccess;
+		}
+
+		if (!m_bStatsValid) return;
+
+		// Get info from sources
+		// Evaluate achievements
+		
+
+		//Store stats in the Steam database if necessary
+		if (m_bStoreStats) {
+			// already set any achievements in UnlockAchievement in "Evaluate achievements"
+			// set stats
+
+			// Send to steam
+			bool bSuccess = SteamUserStats.StoreStats();
+			// If this failed, we never sent anything to the server, try
+			// again later.
+			m_bStoreStats = !bSuccess;
+		}
+	}
+
+
+	//-----------------------------------------------------------------------------
+	// Purpose: Game state has changed
+	//-----------------------------------------------------------------------------
+	public void OnGameStateChange() {
+		if (!m_bStatsValid) return;
+
+		// Update Action
+
+		// We want to update stats the next frame.
+		m_bStoreStats = true;
+	}
+	
+	
+	//-----------------------------------------------------------------------------
+	// Purpose: We have stats data from Steam. It is authoritative, so update
+	//			our data with those results now.
+	//-----------------------------------------------------------------------------
+	private void OnUserStatsReceived(UserStatsReceived_t pCallback) {
+		if (!SteamManager.Initialized)
+			return;
+
+		// we may get callbacks for other games' stats arriving, ignore them
+		if ((ulong)m_GameID == pCallback.m_nGameID) {
+			if (EResult.k_EResultOK == pCallback.m_eResult) {
+				Debug.Log("Received stats and achievements from Steam\n");
+
+				m_bStatsValid = true;
+
+				// load achievements / stats
+				
+			}
+			else {
+				Debug.Log("RequestStats - failed, " + pCallback.m_eResult);
+			}
+		}
+	}
+	
+	//-----------------------------------------------------------------------------
+	// Purpose: Our stats data was stored!
+	//-----------------------------------------------------------------------------
+	private void OnUserStatsStored(UserStatsStored_t pCallback) {
+		// we may get callbacks for other games' stats arriving, ignore them
+		if ((ulong)m_GameID == pCallback.m_nGameID) {
+			if (EResult.k_EResultOK == pCallback.m_eResult) {
+				Debug.Log("StoreStats - success");
+			}
+			else if (EResult.k_EResultInvalidParam == pCallback.m_eResult) {
+				// One or more stats we set broke a constraint. They've been reverted,
+				// and we should re-iterate the values now to keep in sync.
+				Debug.Log("StoreStats - some failed to validate");
+				// Fake up a callback here so that we re-load the values.
+				UserStatsReceived_t callback = new UserStatsReceived_t();
+				callback.m_eResult = EResult.k_EResultOK;
+				callback.m_nGameID = (ulong)m_GameID;
+				OnUserStatsReceived(callback);
+			}
+			else {
+				Debug.Log("StoreStats - failed, " + pCallback.m_eResult);
+			}
+		}
+	}
+
+	//-----------------------------------------------------------------------------
+	// Purpose: An achievement was stored
+	//-----------------------------------------------------------------------------
+	private void OnAchievementStored(UserAchievementStored_t pCallback) {
+		// We may get callbacks for other games' stats arriving, ignore them
+		if ((ulong)m_GameID == pCallback.m_nGameID) {
+			if (0 == pCallback.m_nMaxProgress) {
+				Debug.Log("Achievement '" + pCallback.m_rgchAchievementName + "' unlocked!");
+			}
+			else {
+				Debug.Log("Achievement '" + pCallback.m_rgchAchievementName + "' progress callback, (" + pCallback.m_nCurProgress + "," + pCallback.m_nMaxProgress + ")");
+			}
+		}
+	}
+}
