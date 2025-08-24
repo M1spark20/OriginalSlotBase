621fdf67b91e2ec462525fa3fe01eb15f7f57be3
sin spark <spaaaark.sin120@gmail.com>
2025-08-24 16:17:55 +0900

    [FIX]強制フラグ設定画面をさらに修正
    ボーナスごとにフラグを立てないものを指定できるようにした。
    ボーナス成立後もフラグは立てられるので、成立後は通常成立しないフラグも成立させられる仕様にする。

--- Assets/GameAsset/Script/UI/UISetForceFlag.cs
+++ Assets/GameAsset/Script/UI/UISetForceFlag.cs
@@ -12,6 +12,8 @@ public class UISetForceFlag : MonoBehaviour
 	// #*でリール図柄を、その他で文字を表示。","で分割
 	[SerializeField] private string BonusShow;
 	[SerializeField] private string MinorShow;
+	// X=Bonus, Y=Minorのフラグ組合せを無効化。※ボーナス当選後は出現可能
+	[SerializeField] private Vector2[] UnableSet;
 	// 改行位置の指定
 	[SerializeField] private int ShowReturnPos;
 	
@@ -23,7 +25,10 @@ public class UISetForceFlag : MonoBehaviour
 	private TextMeshProUGUI   txEn;
 	private TextMeshProUGUI[] txB;
 	private TextMeshProUGUI[] txM;
+	private Button[] btB;
+	private Button[] btM;
 	private SlotEffectMaker2023.Action.SystemData sys;
+	private SlotEffectMaker2023.Action.SlotBasicData bas;
 	
 	private void Start(){
 		// 表示内容をデコードする
@@ -36,13 +41,16 @@ public class UISetForceFlag : MonoBehaviour
 		selectorB = new GameObject[BonusCount];
 		imB = new Image[BonusCount];
 		txB = new TextMeshProUGUI[BonusCount];
+		btB = new Button[BonusCount];
 		selectorM = new GameObject[MinorCount];
 		imM = new Image[MinorCount];
 		txM = new TextMeshProUGUI[MinorCount];
+		btM = new Button[MinorCount];
 		imEn = EnableButton.GetComponent<Image>();
 		txEn = EnableButton.transform.Find("IDText").GetComponent<TextMeshProUGUI>();
 		// Singleton初期化
 		sys = SlotEffectMaker2023.Singleton.SlotDataSingleton.GetInstance().sysData;
+		bas = SlotEffectMaker2023.Singleton.SlotDataSingleton.GetInstance().basicData;
 		var comaData = ReelChipHolder.GetInstance();	// Singleton
 		
 		// Instantiateする(Bonus)
@@ -53,6 +61,7 @@ public class UISetForceFlag : MonoBehaviour
 			// 画像を載せて文字を更新する
 			imB[i] = selectorB[i].transform.Find("Image") .GetComponent<Image>();
 			txB[i] = selectorB[i].transform.Find("Button").transform.Find("IDText").GetComponent<TextMeshProUGUI>();
+			btB[i] = selectorB[i].transform.Find("Button").GetComponent<Button>();
 			if (bsBDec[i][0] == '#'){
 				imB[i].enabled = true;
 				imB[i].sprite = comaData.ReelChipDataMini.Extract((int)(bsBDec[i][1] - '0'));
@@ -78,6 +87,7 @@ public class UISetForceFlag : MonoBehaviour
 			// 画像を載せて文字を更新する
 			imM[i] = selectorM[i].transform.Find("Image") .GetComponent<Image>();
 			txM[i] = selectorM[i].transform.Find("Button").transform.Find("IDText").GetComponent<TextMeshProUGUI>();
+			btM[i] = selectorM[i].transform.Find("Button").GetComponent<Button>();
 			if (bsMDec[i][0] == '#'){
 				imM[i].enabled = true;
 				imM[i].sprite = comaData.ReelChipDataMini.Extract((int)(bsMDec[i][1] - '0'));
@@ -90,7 +100,7 @@ public class UISetForceFlag : MonoBehaviour
 			imM[i] = selectorM[i].transform.Find("Button").GetComponent<Image>();
 			
 			// ボタン押下時のスクリプト登録
-        	var prm = i-1;	// 変数に入れないとデルタがうまく動かないらしい…
+        	var prm = i;	// 変数に入れないとデルタがうまく動かないらしい…
         	selectorM[i].transform.Find("Button").GetComponent<Button>().onClick.AddListener(() => OnClickButtonM(prm));
 		}
 	}
@@ -99,16 +109,30 @@ public class UISetForceFlag : MonoBehaviour
 		Color cl = sys.ForceFlagEnable ? Color.yellow : Color.white;
 		imEn.color = cl;
 		txEn.color = cl;
-		
+				
+		bool enFlag = sys.ForceFlagEnable && bas.gameMode == 0;
 		for (int i=0; i<selectorB.Length; ++i){
+			bool enB = enFlag;
+			if (!enB) sys.ForceFlagBonus = -1;
 			selectorB[i].SetActive(sys.ForceFlagEnable);
-			cl = (sys.ForceFlagEnable && sys.ForceFlagBonus == i-1) ? Color.yellow : Color.white;
+			btB[i].interactable = enB;
+			
+			cl = (enB && sys.ForceFlagBonus == i-1) ? Color.yellow : Color.white;
+			if (!enB) cl = Color.gray;
 			imB[i].color = cl;
 			txB[i].color = cl;
 		}
+		
 		for (int i=0; i<selectorM.Length; ++i){
+			// 無効データチェック
+			bool enM = enFlag && sys.ForceFlagBonus >= 0;
+			foreach (var item in UnableSet) enM &= !(item[0] == sys.ForceFlagBonus && item[1] == i);
 			selectorM[i].SetActive(sys.ForceFlagEnable);
-			cl = (sys.ForceFlagEnable && sys.ForceFlagMinor == i-1) ? Color.yellow : Color.white;
+			btM[i].interactable = enM;
+			if (sys.ForceFlagMinor == i && !enM) sys.ForceFlagMinor = -1;
+			
+			cl = (sys.ForceFlagEnable && sys.ForceFlagMinor == i) ? Color.yellow : Color.white;
+			if (!enM) cl = Color.gray;
 			imM[i].color = cl;
 			txM[i].color = cl;
 		}
